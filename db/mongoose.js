9344ff91369c727dee3d9b336ee5b1bc757d7da0
Hamza Hajji <hmzhjji@gmail.com>
2017-09-03 22:13:54 +0100

    configure database for heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-const MongoDB_URI = 'mongodb://localhost:27017/short_urls';
+const MongoDB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/short_urls';
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MongoDB_URI);
